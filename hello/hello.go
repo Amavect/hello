4a2b8b19fc7eddbfa7804c6cd788a0f42d51ecef
Amavect <amavect@gmail.com>
2017-05-15 20:14:56 -0500

    Hello, world!

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	
-	"github.com/Amavect/hello/stringutil"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("asdfjkl;"))
-	fmt.Println(stringutil.Reverse("reverse esrever"))
+	fmt.Println(fmt.Println("Hello, world!")
 }
