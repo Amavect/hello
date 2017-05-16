218e5c7afa84266e275cc5ff58c70ef4b2695558
Amavect <amavect@gmail.com>
2017-05-15 20:10:19 -0500

    initial commit

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	
+	"github.com/Amavect/hello/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("asdfjkl;"))
+	fmt.Println(stringutil.Reverse("reverse esrever"))
+}
